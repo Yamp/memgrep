6949ff9907b25f5b327c1d04645c6e1274fc9b4d
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2023-02-05 21:44:10 +0700

    File name print

--- ocr_extract.py
+++ ocr_extract.py
@@ -4,6 +4,8 @@ from PIL import Image
 
 import pytesseract
 
+# https://www.google.com/search?q=easy_tesseract&newwindow=1&sxsrf=AJOqlzWqC3xhLtgG1mMbi3f2UaINveYIxQ%3A1675606506042&ei=6rnfY6mdAtvJz7sP3M6g0AQ&ved=0ahUKEwipus3UyP78AhXb5HMBHVwnCEoQ4dUDCA8&uact=5&oq=easy_tesseract&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIGCAAQHhAPMgYIABAIEB4yCAgAEAgQHhAKMgUIABCGAzIFCAAQhgM6CggAEEcQ1gQQsAM6BAgjECc6BQgAEJECOgUIABCABDoHCCMQ6gIQJzoLCC4QgwEQsQMQgAQ6CwgAEIAEELEDEIMBOggIABCABBCxAzoLCC4QsQMQgwEQ1AI6BAgAEEM6CggAELEDEIMBEEM6EAgAEIAEEBQQhwIQsQMQgwE6EQguEIAEELEDEIMBEMcBENEDOhAILhCxAxCDARDHARDRAxBDOgsILhCABBCxAxDUAjoKCAAQgAQQFBCHAjoHCAAQgAQQCjoNCC4QgAQQxwEQ0QMQCjoECAAQHjoICAAQCBAeEA9KBAhBGABKBAhGGABQ_tpXWMiTWGDolFhoA3ABeACAAb0CiAHRFJIBCDAuMTQuMS4xmAEAoAEBsAEKyAEIwAEB&sclient=gws-wiz-serp
+
 folder = Path(__file__).parent / 'images'
 # iterate over all files in the folder
 for file in folder.iterdir():
@@ -12,8 +14,9 @@ for file in folder.iterdir():
         # open the image
         image = Image.open(file)
         # convert the image to a string
-        text = pytesseract.image_to_string(image, lang='ru')
+        text = pytesseract.image_to_string(image, lang='rus')
         # print the text
+        print(file.name)
         print(text)
 
 print(pytesseract.image_to_string('test.png'))
