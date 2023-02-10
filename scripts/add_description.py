7813fcc689763baf0b75f28000d6e464d507d4f2
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2023-02-11 03:55:39 +0700

    Redis = None

--- scripts/add_description.py
+++ scripts/add_description.py
@@ -9,7 +9,6 @@ from loguru import logger
 
 from data.data_storage import DataStorage
 from data.pg.postgre_db import PostgresDB
-from data.redis_db import RedisDB
 from data.s3_db import S3DB
 from extraction.caption import ImageCaptioner
 
@@ -25,13 +24,13 @@ def recognise_image(id: str, data: bytes) -> str:
 def main():
     logger.info("Initializing s3...")
     s3 = S3DB()
-    logger.info("Initializing redis...")
-    redis = RedisDB()
+    # logger.info("Initializing redis...")
+    # redis = RedisDB()
     logger.info("Initializing postgres...")
     pg = PostgresDB()
 
     logger.info("Creating storage...")
-    storage = DataStorage(s3, redis, pg)
+    storage = DataStorage(s3, None, pg)
     res = storage.get_all_images()
 
     for id, img in res.items():
