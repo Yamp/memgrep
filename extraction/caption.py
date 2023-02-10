bd90fa00704bc320ed9b35a12f37a4881a26ab7e
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2023-02-11 04:01:33 +0700

    image_path

--- extraction/caption.py
+++ extraction/caption.py
@@ -14,7 +14,7 @@ class ImageCaptioner:
         self.model, self.vis_processors, _ = load_model_and_preprocess(
             name=model_name, model_type=model_type, is_eval=True, device=self.device)
 
-    def caption(self, image: Path) -> str:
+    def caption(self, image_path: Path) -> str:
         raw_image = Image.open(image_path).convert("RGB")
         image = self.vis_processors["eval"](raw_image).unsqueeze(0).to(self.device)
         caption_str = self.model.generate({"image": image})
