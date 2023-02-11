48bcdfeaf92dc542db24f7622dcd00d8a8dd9de9
Victor Golovnin <victor.golovnin@gmail.com>
2023-02-12 06:20:39 +0700

    uncomment extra

--- data/redis_db.py
+++ data/redis_db.py
@@ -216,9 +216,9 @@ class RedisDB:
         records = []
         for doc in res.docs:
             d = doc.__dict__  # noqa
-            # d["semantic_vector"] = parse_list(d["semantic_vector"])
-            # d["reactions"] = parse_list(d["reactions"])
-            # d["comments"] = parse_list(d["comments"])
+            d["semantic_vector"] = parse_list(d["semantic_vector"])
+            d["reactions"] = parse_list(d["reactions"])
+            d["comments"] = parse_list(d["comments"])
             d["id"] = int(d["id"].removeprefix("doc:"))
             records += [ImageRecord(**d)]
 
