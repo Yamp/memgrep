6e04e4f2ba23f2b9c1ff6b5f32fec29a96823317
Victor Golovnin <victor.golovnin@gmail.com>
2023-02-12 00:19:10 +0700

    turn off bot index

--- scripts/start_api.py
+++ scripts/start_api.py
@@ -10,12 +10,12 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 import settings
-from data.minio_db import MinioDB
+# from data.minio_db import MinioDB
 from data.redis_db import RedisDB, SearchRequest
 from scraper.tg import TelegramScraper
 
 index = RedisDB()
-storage = MinioDB()
+# storage = MinioDB()
 
 
 async def search_memes(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -32,11 +32,11 @@ async def search_memes(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text(f"Hello {res}")
 
 
-async def index_memes(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Index a chat."""
-    scraper = TelegramScraper(storage, index)
-    await scraper.scrape_messages(update.message.text)
-    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+# async def index_memes(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Index a chat."""
+#     scraper = TelegramScraper(storage, index)
+#     await scraper.scrape_messages(update.message.text)
+#     await update.message.reply_text(f"Hello {update.effective_user.first_name}")
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -47,7 +47,7 @@ def main():
     app = ApplicationBuilder().token(settings.TG_BOT_TOKEN).build()
     app.add_handler(CommandHandler("hello", hello))
     app.add_handler(CommandHandler("search", search_memes))
-    app.add_handler(CommandHandler("index", index_memes))
+    # app.add_handler(CommandHandler("index", index_memes))
     app.run_polling()
 
 
