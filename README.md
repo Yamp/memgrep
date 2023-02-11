9f3e1de9873d8ecd3a3023a0841a9dbf2638ac7a
Kirill Korikov <korikov.kirill@gmail.com>
2023-02-11 09:12:26 +0300

    update reqs and ocr demo

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 A Python project for indexing and searching memes using OCR, fuzzy search and semantic search.
 
-# Installation
-0. Clone this repository: `git clone https://github.com/Yamp/memgrep`
-1. Navigate to the cloned repository: `cd memgrep`
-
-## Reqirements
+# Installation (ubuntu)
 0. Install rust `curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`
-1. (Ubuntu) `sudo apt install python3.10-full`
-2. Create new venv and activate it: `python3.10 -m venv venv && source venv/bin/activate`
-3. Install requirements: `pip3 install -r requirements.txt`
+1. Install python 3.10 `sudo apt install python3.10-full`
+2. Install pytesseract deps: `sudo apt install libleptonica-dev tesseract-ocr libtesseract-dev python3-pil tesseract-ocr-rus  tesseract-ocr-eng tesseract-ocr-script-latn tesseract-ocr-script-cyrl`
+3. Create new venv and activate it: `python3.10 -m venv venv && source venv/bin/activate`
+4. Install requirements: `pip3 install -r requirements.txt`
 
 ## Poetry (TBD)
 **
--- extraction/ocr.py
+++ extraction/ocr.py
@@ -1,5 +1,7 @@
-import warnings
+import os
 from pathlib import Path
+from pprint import pprint
+import warnings
 
 from PIL import Image
 from pytesseract import pytesseract
@@ -20,6 +22,17 @@ class OCRExtractor:
         s = "".join([c for c in s if c.isalpha() or c.isdigit() or c == " "])
         s = s.lower()
         return s
+
+    def extract_dir(self, image_dir: Path) -> dict:
+        if not os.path.isdir(image_dir):
+            warnings.warn(f'Not a valid dir: {image_dir}')
+            return {}
+        result = {}
+        for image_path in os.listdir(image_dir):
+            if image_path.endswith('.jpg'):
+                image_name = image_path[:-len('.jpg')]
+                result[image_name] = self.extract(os.path.join(image_dir, image_path))
+        return result
 
     def extract_tesseract(
             self,
@@ -42,3 +55,15 @@ class OCRExtractor:
 
         reader = easyocr.Reader(["ru", "en"])
         return reader.readtext(Image.open(image))
+
+
+if __name__ == '__main__':
+    ocr = OCRExtractor()
+    while True:
+        image_path = input("Enter image(s) path: ")
+        if not os.path.exists(image_path):
+            pprint(f'Incorrect path {image_path}')
+        elif os.path.isdir(image_path):
+            pprint(ocr.extract_dir(image_path))
+        else:
+            pprint(ocr.extract(image_path))
